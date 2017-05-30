010d244d57eceb0578345900b8aeef0b6f0c5018
dearzhaorui <s231720@studenti.polito.it>
2017-05-30 13:42:24 +0200

    First change

--- Conti/nf-dev-master-e6d0e0e33115819338cf2f4cab229d682a635242/src/it/polito/nfdev/lib/Packet.java
+++ Conti/nf-dev-master-e6d0e0e33115819338cf2f4cab229d682a635242/src/it/polito/nfdev/lib/Packet.java
@@ -21,7 +21,13 @@ public class Packet {
 	public static final String HTTP_REQUEST = "HTTP_REQ";
 	public static final String HTTP_RESPONSE = "HTTP_RESP";
 	public static final String POP3_REQUEST = "POP3_REQ";
-	public static final String POP3_RESPONSE = "POP3_RESP";
+	public static final String POP3_RESPONSE = "POP3_RESP";  // if app_protocol=pop3_resp && l7data contains specified keyword, must DROP the packet
+	public static final String DNS_REQUEST = "DNS_REQ";
+	public static final String DNS_RESPONSE = "DNS_RESP";
+	
+	public static final String L2_TCP = "TCP";
+	public static final String L2_UDP = "UDP";
+	
 	
 	private Map<PacketField, String> fields;
 	
